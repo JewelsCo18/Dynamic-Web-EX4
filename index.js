d6bc965d2736e768fb4834dfdce32aea531ab87b
Jules Kho <juleskho@MacBook-Pro.lan>
2021-11-04 00:20:31 -0400

    adding firebase

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express"); 
 const app = express(); 
+//Setting up port dynaically with Heroku
+const port = process.env.PORT || 4000; 
 
-const port = 4000; 
+const firebase = require("firebase/app"); 
+
+//Firebase configuration object 
+const firebaseConfig = {
+    apiKey: "AIzaSyAM9jTOzVw5P6ltKUsW6N_tj-65Xi1Dulw",
+    authDomain: "exercise-four-af84d.firebaseapp.com",
+    projectId: "exercise-four-af84d",
+    storageBucket: "exercise-four-af84d.appspot.com",
+    messagingSenderId: "181653130447",
+    appId: "1:181653130447:web:64bac580d11d2251a1fbba"
+};
+
+firebase.initializeApp(firebaseConfig); 
 
 const indexRoute = require('./routes/index.js'); 
 const articleRoute = require('./routes/article')
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const router = express.Router(); 
 
+const firebase = require("firebase/firestore/lite"); 
+
+const db = firebase.getFirestore(); 
+//reference to the blogpost collection created on firebase web 
+const blogposts = db.collection("blogpost"); 
+
 router.get('/', (req, res) =>{
     res.send(`
     <h1> All Articles</h1>
